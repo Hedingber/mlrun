31779b2f9518cfdc65b3526cd31f17e9fb584f43
Hedi Ingber <hedingber@gmail.com>
2021-01-18 22:42:17 +0200

    Add felix monitoring

--- mlrun/api/api/endpoints/monitoring.py
+++ mlrun/api/api/endpoints/monitoring.py
@@ -2,8 +2,12 @@ import fastapi
 import datetime
 import linecache
 import pathlib
+import objgraph
+import gc
 
 import mlrun
+import time
+import mlrun.errors
 import mlrun.api.utils.periodic
 import tracemalloc
 
@@ -19,7 +23,74 @@ async def enable_memory_monitoring(
 ):
     logger.info("Enabling memory monitoring", interval=interval, number_of_frames=number_of_frames)
     tracemalloc.start(number_of_frames)
-    mlrun.api.utils.periodic.run_function_periodically(interval, _run_memory_monitoring)
+    # mlrun.api.utils.periodic.run_function_periodically(interval, _run_memory_monitoring)
+    mlrun.api.utils.periodic.run_function_periodically(interval, _generate_memory_sample)
+
+
+@router.post("/monitoring/memory/sample")
+def sample_memory_monitoring(
+    object_type: str = None,
+    start_index: int = None,
+    sample_size: int = None,
+    create_graph: bool = False,
+    max_depth: int = 3,
+):
+    _generate_memory_sample(object_type, start_index, sample_size, create_graph, max_depth)
+
+
+def _generate_memory_sample(
+    object_type: str = None,
+    start_index: int = None,
+    sample_size: int = None,
+    create_graph: bool = False,
+    max_depth: int = 3,
+):
+    logger.debug("Generating memory sample")
+    gc.collect()
+    if object_type is not None and (start_index is not None or (sample_size is not None and sample_size > 0)):
+
+        requested_objects = objgraph.by_type(object_type)
+        sample_size = sample_size or 1
+
+        # If 'start_index' not given use 'sample_size' to calculated it from the end of the list
+        if start_index is None or start_index < 0:
+            start_index = len(requested_objects) - sample_size
+
+        if start_index < len(requested_objects):
+
+            # Iterate until 'sample_size' or the end of the list is reached
+            for object_index in range(start_index, min(start_index + sample_size, len(requested_objects))):
+                logger.debug('Requested object',
+                                   object_type=object_type,
+                                   object_index=object_index,
+                                   total_objects=len(requested_objects),
+                                   requested_object=str(requested_objects[object_index])[:10000])
+
+                if create_graph:
+                    logger.debug('Creating reference graph',
+                                       object_index=object_index,
+                                       max_depth=max_depth)
+                    _create_object_ref_graph(object_type,
+                                                  requested_objects[object_index],
+                                                  object_index,
+                                                  max_depth=max_depth)
+
+        else:
+            message = 'Object start index is invalid'
+            logger.warn(message,
+                               object_type=object_type,
+                               start_index=start_index,
+                               total_objects=len(requested_objects))
+            raise mlrun.errors.MLRunBadRequestError(message)
+    else:
+        logger.debug('Most common objects', most_common=str(objgraph.most_common_types()))
+
+
+def _create_object_ref_graph(object_type, object_, object_index, max_depth=3):
+    datetime_string = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H_%M_%S')
+    filename = f'{object_type}-{object_index}-{datetime_string}.dot'
+    object_ref_graphs_dir = pathlib.Path("/mlrun/db/object-ref-graphs")
+    objgraph.show_backrefs(object_, filename=str(object_ref_graphs_dir / filename), refcounts=True, max_depth=max_depth)
 
 
 def _run_memory_monitoring():
